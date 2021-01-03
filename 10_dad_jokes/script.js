b866256baef3bcba6000a18106d5d0edf8b5e639
vault-glitch <chrisdavis1083@gmail.com>
2021-01-02 20:41:46 -0800

    commit on main

---
+++ 10_dad_jokes/script.js
@@ -0,0 +1,23 @@
+const jokeEl = document.getElementById("joke")
+const jokeBtn = document.getElementById("jokeBtn")
+
+jokeBtn.addEventListener("click", generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+    const config = {headers: {"Accept": "application/JSON"}}
+    const res = await fetch("https://icanhazdadjoke.com/", config)
+    const data = await res.json()      
+    jokeEl.innerHTML = data.joke
+}
+
+// function generateJoke() {
+//     const config = {headers: {"Accept": "application/JSON"}
+//     }
+//     fetch("https://icanhazdadjoke.com/", config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
